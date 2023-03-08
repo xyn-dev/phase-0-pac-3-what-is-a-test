4be2766f073fd09cc69a6117a0db243a7c3b1837
Hunter Wade <calebh.wade@gmail.com>
2023-03-08 16:21:55 -0500

    complete lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,38 +1,20 @@
-require ( './root.js' );
+const { name, height, message } =require("../index.js");
 
-const { name, height, message } = require("../index.js");
+describe("Name", () => {
+  it('returns "Susan"', () => {
+    expect(name).toEqual("Susan");
+  });
+});  
 
-/*
-describe('what-is-a-test', () => {
-  describe('Name', () => {
-    it('returns "Susan"', () => {
-      expect(name).toEqual('Susan')
-    })
-  })
+  describe("Height", () => {
+    it("is less than 40", () => {
+      expect(height).toBeLessThan(40);
+    });
+  });
 
-
-  describe('Height', () => {
-    it('is less than 40', () => {
-      expect(height).toBeLessThan(40)
-    })
-  })
-
-  describe('Message', () => {
-    it('gives the name and height', () => {
-      expect(message).toInclude(name)
-      expect(message).toInclude(height)
-    })
-  })
-})
-*/
-
-// The code below ensures that students who are using CodeGrade will get credit 
-// for the code-along in Canvas; you can disregard it.
-
-describe('', () => {
-  describe('', () => {
-    it('', () => {
-      return true;
-    })
-  })
-})
+  describe("Message", () => {
+      it("gives the name and height", () => {
+        expect(message).toInclude(name);
+        expect(message).toInclude(height);
+      });
+    });
